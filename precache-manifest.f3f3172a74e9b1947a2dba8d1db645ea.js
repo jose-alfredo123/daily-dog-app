0fb679da28eea366d87ec73a1a895055a41954d0
josealfredoRG <jose12pedo@gmail.com>
2019-09-19 09:20:39 -0600

    Updates

---
+++ precache-manifest.f3f3172a74e9b1947a2dba8d1db645ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b527efb2c23e513c20ca1273fc8e191",
+    "url": "/daily-dog-app/index.html"
+  },
+  {
+    "revision": "cf4e8fb3a166cbf50cf1",
+    "url": "/daily-dog-app/static/css/main.ebe44358.chunk.css"
+  },
+  {
+    "revision": "71ac389e582aad31ef8c",
+    "url": "/daily-dog-app/static/js/2.6e43e534.chunk.js"
+  },
+  {
+    "revision": "cf4e8fb3a166cbf50cf1",
+    "url": "/daily-dog-app/static/js/main.1957a76f.chunk.js"
+  },
+  {
+    "revision": "e6987d431b5eb6981699",
+    "url": "/daily-dog-app/static/js/runtime~main.1661b829.js"
+  },
+  {
+    "revision": "ddd81f622d2dc6feb3e9c8f2e2588e5a",
+    "url": "/daily-dog-app/static/media/dog_and_cat_pattern-21.ddd81f62.jpg"
+  }
+]);
